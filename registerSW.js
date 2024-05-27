58a35014f4b87af4b8dace2be8df07874c79448a
Yolo <1144318071@qq.com>
2024-05-27 18:29:51 +0800

    feat:hot

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/hot/sw.js', { scope: '/hot/' })})}
